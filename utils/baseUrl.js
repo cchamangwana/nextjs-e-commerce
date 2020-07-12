0bc36e1159d52cf345d4bf202af2daac0bf57d04
Chamangwana <67837544+Chamangwana@users.noreply.github.com>
2020-07-12 20:56:09 +0200

    Update baseUrl.js
    Updated baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -6,7 +6,7 @@ export default baseUrl;
 */
 
 const baseUrl = process.env.NODE_ENV === "production"
-    ? 'https://nextjs-kfbkxffzq.vercel.app'
+    ? 'http://localhost:3000'
     : 'http://localhost:3000';
 
 export default baseUrl;
